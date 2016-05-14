1177c60f4ae3b591719dddc7c547da341eb10103
Samnang <samnangsuon.ss@gmail.com>
2016-05-13 21:01:00 -0400

    Version du 13 Mai 2016 (21h00) Par SUON, Samnang
    + Dans toutes les vues, toujours utiliser le code suivant :
    
    if (!ISSET($_SESSION)) session_start();

--- controleur/SauvegarderAction.class.php
+++ controleur/SauvegarderAction.class.php
@@ -8,9 +8,7 @@ require_once('./modele/EmployeurDAO.class.php');
 class SauvegarderAction implements Action {
     public function execute() {
 
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
+        if (!ISSET($_SESSION)) session_start();
         if(isset($_REQUEST['role']) && $_REQUEST['role'] == 'employeur') {
             $Employeur = new Employeur();
             $Employeur->setNomUser($_SESSION['connecte']);
